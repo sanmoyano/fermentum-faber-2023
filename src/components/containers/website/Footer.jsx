bee458ef881b8a74c07183cf906fd98024b39dbe
sanmoyano <smoyano.di@gmail.com>
2022-12-02 13:02:23 -0300

    Footer

--- src/components/containers/website/Footer.jsx
+++ src/components/containers/website/Footer.jsx
@@ -2,8 +2,17 @@ import { Stack, Text } from "@chakra-ui/react";
 
 const Footer = () => {
     return (
-        <Stack bgColor={"gray.500"} width={"100%"}>
-            <Text>Footer</Text>
+        <Stack
+            alignItems={"center"}
+            bgColor={"brand.500"}
+            justifyContent={"center"}
+            p={6}
+            width={"100%"}
+        >
+            <Text textAlign={"center"} textStyle={"body"}>
+                2021 - Fermentum Faber, Córdoba Argentina. Todos los derechos reservados. Disfrutar
+                responsablemente. Prohibida su venta a menores de 18 años.
+            </Text>
         </Stack>
     );
 };
