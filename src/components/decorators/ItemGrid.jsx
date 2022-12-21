20d0cb5971119a9ea0a57af5e93197812adfe51b
sanmoyano <smoyano.di@gmail.com>
2022-12-21 11:43:40 -0300

    update card info

--- src/components/decorators/ItemGrid.jsx
+++ src/components/decorators/ItemGrid.jsx
@@ -1,26 +1,30 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
 
 const ItemGrid = ({ img, title, description }) => {
     return (
-        <Stack
-            alignItems={"center"}
-            bgImage={img}
-            bgPosition={"center"}
-            bgSize={"cover"}
-            borderRadius={6}
-            height={"100%"}
-            justifyContent={"center"}
-            p={4}
-        >
+        <Stack alignItems={"center"} height={"100%"} justifyContent={"center"}>
+            <Stack
+                alignItems={"center"}
+                bgImage={img}
+                bgPosition={"center"}
+                bgSize={"cover"}
+                borderRadius={6}
+                height={"100%"}
+                justifyContent={"center"}
+                p={4}
+            >
+                <Text color={"brand.200"} textAlign={"center"} textStyle={"heading"}>
+                    {description}
+                </Text>
+            </Stack>
             <Text
                 color={"brand.500"}
                 textAlign={"center"}
-                textDecoration={"none"}
                 textStyle={"heading"}
+                textTransform={"uppercase"}
             >
                 {title}
             </Text>
-            <Text textAlign={"center"}>{description}</Text>
         </Stack>
     );
 };
