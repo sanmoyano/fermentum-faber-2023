9fbeffeb3b26cf58c3fb57954840dc4ae86d61fe
sanmoyano <smoyano.di@gmail.com>
2023-01-13 10:03:20 -0300

    Toggle menu

--- src/components/containers/website/components/navbar/MenuToggle.jsx
+++ src/components/containers/website/components/navbar/MenuToggle.jsx
@@ -3,10 +3,16 @@ import { HiMenu } from "react-icons/hi";
 import { IoMdClose } from "react-icons/io";
 
 const MenuToggle = ({ open, toggle }) => {
+    const sizeIcon = {
+        size: "30px",
+    };
+
     return (
         <IconButton
+            aria-label={"toggle menu"}
             color={"brand.200"}
-            icon={open ? <IoMdClose /> : <HiMenu />}
+            icon={open ? <IoMdClose {...sizeIcon} /> : <HiMenu {...sizeIcon} />}
+            size={"lg"}
             variant={"ghost"}
             onClick={toggle}
         />
